fb7a445861946c0b9bd47c363a3a911c6c5bc8e1
ruth-turc <ruth.turc@gmail.com>
2025-10-20 21:30:03 +0000

    Function Prototypes

--- textAdventure.c
+++ textAdventure.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* PROTOTYPES */
+int calculatePlayerBonus(const char inventory[]);
+int playerCombatNumber(int playerBonus);
+int monsterCombatNumber(int floor level);
+int goldPickUp(int floor level);
+
+/* global variables */
+int playerHP;
+
 /* Maps!
         Key: 'u' = stairs up, 'd' = stairs down,'g' = gold, 
             'm' = monster, 'i' = item, 'v' = vender/merchant
+
+    We might want to eventually move these to their own file 
+    and use a pointer?
 */
 char floor1[][] = {' ','m','g'}
                   {' ',' ','d'};
 
 char floor2[][] = {' ','i','d'}
                   {'u','m',' '}
-                  {' ',' ','g'}
+                  {' ',' ','g'};
 int main(){
-
+    char inventory[10];
+    int playerHP = 100;
     return 0;
 }
 
