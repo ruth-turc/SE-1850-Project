56e59093ae276b43e9a32874138f46f91515d842
ruth-turc <ruth.turc@gmail.com>
2025-10-20 21:18:23 +0000

    Created Floor 1-2

--- textAdventure.c
+++ textAdventure.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+/* Maps!
+        Key: 'u' = stairs up, 'd' = stairs down,'g' = gold, 
+            'm' = monster, 'i' = item, 'v' = vender/merchant
+*/
+char floor1[][] = {' ','m','g'}
+                  {' ',' ','d'};
 
+char floor2[][] = {' ','i','d'}
+                  {'u','m',' '}
+                  {' ',' ','g'}
 int main(){
 
     return 0;
