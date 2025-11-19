c9dba956d71f9ff9851d41e3caa3e8ee3c5ce852
enaz-glitch <enaz@iastate.edu>
2025-11-19 15:42:23 -0600

    map files

---
+++ textAMaps.h
@@ -0,0 +1,15 @@
+#ifndef TEXTAMAPS_H
+#define TEXTAMAPS_H
+
+typedef struct {
+    int row;
+    int col;
+    char** floor; //pointer to array of pointers to arrays of chars, first * is to a row and second * is to a column 
+} map;
+
+map* createFloor(int row, int col);
+void initFloors(map* levels[]);
+void fillFloor(map* levels[], int floorNum,int col, char temp[][col]);
+void freeFloor(map* map);
+
+#endif 
